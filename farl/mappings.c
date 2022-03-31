77f5c99c4015c6ff52f803de0b1f8efa1c6e80f8
surilindur <surilindur@users.noreply.github.com>
2022-03-31 18:54:28 +0300

    attempt to fix memory leak

--- farl/mappings.c
+++ farl/mappings.c
@@ -33,14 +33,16 @@ bool node_free_string_memory(const void* a, void* udata)
 char* resolve_path(char* path)
 {
     char* output;
+    char resolved_path[PATH_MAX];
 
-    if ((output = realpath(path, NULL)) == NULL)
+    if (realpath(path, resolved_path) != NULL)
     {
-        free(output);
-        size_t path_length = strlen(path) + 1;
-        output = (char*)malloc(path_length * sizeof(char));
-        strncpy(output, path, path_length);
+        path = resolved_path;
     }
+
+    size_t path_length = strlen(path) + 1;
+    output = (char*)malloc(path_length * sizeof(char));
+    strncpy(output, path, path_length);
 
     #ifdef FARL_LOG_PATHS
     lprintf("path: \"%s\" -> \"%s\"", path, output);
